d8195ff2d6c74b0b72de43478fe62318f03800d0
123kieran <123kieran@gmail.com>
2017-12-01 14:55:51 +0000

    Q2

--- ElizaResponse.go
+++ ElizaResponse.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"regexp"
 	"time"
 )
 
-func elizaResponse(inputStr string) string {
-	//return random string output
-	input := inputStr
+func elizaResponse(userInput string) string {
+	input := userInput
+	pattern := `(?i).*\bFather\b.*`
+	output := "Why don’t you tell me more about your father?"
+	response := ""
 	rand.Seed(time.Now().UTC().UnixNano())
 	answers := []string{
 		"I’m not sure what you’re trying to say. Could you explain it to me?",
@@ -16,12 +19,17 @@ func elizaResponse(inputStr string) string {
 		"Why do you say that?",
 	}
 
-	response := "User :" + input + " \n Eliza :" + answers[rand.Intn(len(answers))]
+	//check if pattern is fouund in string
+	if matched, _ := regexp.MatchString(pattern, input); matched {
+		response = "User :" + input + " \nEliza :" + output
+	} else {
+		response = "User :" + input + " \nEliza :" + answers[rand.Intn(len(answers))]
+	}
 	return response
 } //elizaResponse
 
 func main() {
-	// inputs put into array
+	//slice of inputs
 	userInput := []string{
 		"People say I look like both my mother and father.",
 		"Father was a teacher.",
